d377ec7dd99ecd5d1da6595c02e1700808b4d342
Faustine Li <faustineli12@gmail.com>
2016-10-27 16:32:22 -0400

    better comments

--- app.R
+++ app.R
@@ -39,37 +39,39 @@ ui = fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to simulate socks and draw a density plot
 server = function(input, output) {
 
     sock_sim = reactive({
         set.seed(1)
         n_samp = 2000
         replicate(n_samp, {
-            # generating a sample of the parameters from the priors
+
+            # input parameters
             prior_mean = input$prior_mean
             prior_sd   = input$prior_sd
             n_pick     = input$n_pick
             mu_pair    = input$frac_pair
             sig_pair   = (0.1)^2
 
+            # calculating parameter values
             prior_size = -prior_mean^2 / (prior_mean - prior_sd^2)
             a = ((1 - mu_pair) / (sig_pair^2) - 1/mu_pair) * (mu_pair)
             b = a * (1/mu_pair - 1)
 
+            # generating a sample of the parameters from the priors
             n_socks    = rnbinom(1, mu = prior_mean, size = prior_size)
             frac_pair  = rbeta(1, a, b)
             n_pairs    = round(floor(n_socks / 2) * frac_pair)
             n_odd      = n_socks - n_pairs * 2
 
             # simulating picking out n_picked socks
-            socks      = rep(seq_len(n_pairs + n_odd),
-                             rep(c(2, 1), c(n_pairs, n_odd)))
+            socks        = rep(seq_len(n_pairs + n_odd),
+                               rep(c(2, 1), c(n_pairs, n_odd)))
             picked_socks = sample(socks, size =  min(n_pick, n_socks))
             sock_counts  = table(picked_socks)
 
-            # Returning the parameters and counts of the number of matched
-            # and unique socks among those that were picked out.
+            # returning a vector of results
             c(unique  = sum(sock_counts == 1),
               pairs   = sum(sock_counts == 2),
               n_socks = n_socks,
@@ -79,12 +81,14 @@ server = function(input, output) {
         })
     })
 
+    # accept samples that match the data
     post_samp = reactive({
         post = sock_sim()
         n_pick = input$n_pick
         post = post[ , post["unique",] == n_pick]
     })
 
+    # plot density of the number of socks
     output$distPlot = renderPlot({
         post  = post_samp()
         socks = data.frame(n = post['n_socks',])
@@ -99,8 +103,7 @@ server = function(input, output) {
                   axis.title = element_text(size=13),
                   plot.title = element_text(size=20),
                   axis.ticks = element_line(color='gray80'),
-                  legend.position="none"
-            ) +
+                  legend.position="none" ) +
             labs(y = 'Density',
                  x = 'Number of Socks',
                  title = 'Posterior Distribution of Socks')
