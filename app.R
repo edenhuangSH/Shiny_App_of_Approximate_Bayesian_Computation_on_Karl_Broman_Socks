6a804f2997ac0a73190af720e856010fee2d314d
Faustine Li <faustineli12@gmail.com>
2016-10-27 22:36:33 -0400

    added prior plots

--- app.R
+++ app.R
@@ -34,13 +34,17 @@ ui = fluidPage(
 
         mainPanel(
             tabsetPanel(type = 'tabs',
-                tabPanel('Description', htmlOutput('descript'),
+                tabPanel('Description',
+                         htmlOutput('descript'),
                          actionButton('show_button', 'Show Socks'),
                          conditionalPanel('input.show_button > 0',
                                           HTML('<br>'),
                                           verbatimTextOutput('answer'))),
-                tabPanel('Prior Plot', plotOutput('prior_plot')),
-                tabPanel('Posterior Plot', plotOutput('post_plot'))
+                tabPanel('Prior Plots',
+                         plotOutput('prior_nbinom_plot'),
+                         plotOutput('prior_beta_plot')),
+                tabPanel('Posterior Plot',
+                         plotOutput('post_plot'))
 
             )
         )
@@ -64,7 +68,7 @@ server = function(input, output) {
 
             # calculating parameter values
             prior_size = -prior_mean^2 / (prior_mean - prior_sd^2)
-            a = ((1 - mu_pair) / (sig_pair^2) - 1/mu_pair) * (mu_pair)
+            a = ((1 - mu_pair) / (sig_pair) - 1/mu_pair) * (mu_pair)
             b = a * (1/mu_pair - 1)
 
             # generating a sample of the parameters from the priors
@@ -96,8 +100,37 @@ server = function(input, output) {
         post = post[ , post["unique",] == n_pick]
     })
 
-    # plot prior distributions
-    output$prior_plot = renderPlot({
+    # plot prior beta distribution
+    output$prior_beta_plot = renderPlot({
+        mu_pair    = input$frac_pair
+        sig_pair   = (0.1)^2
+        a = ((1 - mu_pair) / (sig_pair) - 1/mu_pair) * (mu_pair)
+        b = a * (1/mu_pair - 1)
+        range = seq(0,1,0.01)
+
+        beta = dbeta(seq(0,1,0.01), a, b)
+        ggplot(data.frame(x = range, y = beta), aes(x = x, y = y)) +
+            geom_line() +
+            theme_bw() +
+            labs(x = 'Fraction of paired socks',
+                 y = 'Density',
+                 title = 'Beta Prior')
+    })
+
+    # plot prior nbinom distribution
+    output$prior_nbinom_plot = renderPlot({
+        prior_mean = input$prior_mean
+        prior_sd   = input$prior_sd
+        prior_size = -prior_mean^2 / (prior_mean - prior_sd^2)
+        range = seq(1,80,1)
+
+        binom = dnbinom(range, mu = prior_mean, size = prior_size)
+        ggplot(data.frame(x = range, y = binom), aes(x = x, y = y)) +
+            geom_line() +
+            theme_bw() +
+            labs(x = 'Total number of Socks',
+                 y = 'Density',
+                 title = 'Negative Binomial Prior')
 
     })
 
@@ -107,16 +140,7 @@ server = function(input, output) {
         socks = data.frame(n = post['n_socks',])
         ggplot(socks, aes(x = n)) +
             geom_density() +
-            theme(rect = element_rect(fill='#F0F0F0', colour='#F0F0F0'),
-                  text = element_text(face='bold'),
-                  panel.grid.major = element_line(color='gray80', size=0.6),
-                  panel.grid.minor = element_blank(),
-                  panel.background = element_rect(fill='#F0F0F0'),
-                  axis.text =  element_text(size=13),
-                  axis.title = element_text(size=13),
-                  plot.title = element_text(size=20),
-                  axis.ticks = element_line(color='gray80'),
-                  legend.position="none" ) +
+            theme_bw() +
             labs(y = 'Density',
                  x = 'Number of Socks',
                  title = 'Posterior Distribution of Socks')
@@ -124,44 +148,43 @@ server = function(input, output) {
     })
 
     output$descript = renderUI({
-        HTML('<br><br>
-             <b>Approximate Bayesian Computing</b><br><br>
+        HTML(
+            '<br><b>Approximate Bayesian Computing</b><br>
 
-             A computational method of estimating the posterior of a
-             distribution when the likelihood cannot easily be derived. From
-             a generative model, samples are simulated, but only the samples
-             which conform to the observed data are accepted. The posterior
-             parameters are estimated from the accepted sample parameters.
-             More information about ABC can be found in this
-             <a href=https://en.wikipedia.org/wiki/Approximate_Bayesian_computation>
-             Wikipedia</a> article. <br><br>
+            A computational method of estimating the posterior of a
+            distribution when the likelihood cannot easily be derived. From
+            a generative model, samples are simulated, but only the samples
+            which conform to the observed data are accepted. The posterior
+            parameters are estimated from the accepted sample parameters.
+            More information about ABC can be found in this
+            <a href=https://en.wikipedia.org/wiki/Approximate_Bayesian_computation>
+            Wikipedia</a> article. <br><br>
 
-             <b>Karl Broman\'s Socks</b><br><br>
+            <b>Karl Broman\'s Socks</b><br>
 
-             The statistician <a href=https://twitter.com/kwbroman/status/523221976001679360>Karl Broman</a> tweeted that of the first 11 socks picked from his dryer are
-             unique. Can we estimate the number of total socks in Broman\'s
-             dryer? A solution adapted from <a href=http://www.sumsar.net/blog/2014/10/tiny-data-and-the-socks-of-karl-broman>Rasmus Baath</a>.
-             uses ABC. Play
-             around with the prior parameters and see how the number of socks
-             change. <br><br>
+            The statistician <a href=https://twitter.com/kwbroman/status/523221976001679360>Karl Broman</a> tweeted that of the first 11 socks picked from his dryer are
+            unique. Can we estimate the number of total socks in Broman\'s
+            dryer? A solution adapted from <a href=http://www.sumsar.net/blog/2014/10/tiny-data-and-the-socks-of-karl-broman>Rasmus Baath</a>.
+            uses ABC. Play around with the prior parameters and see how the
+            distribution of socks change. <br><br>
 
-             <b>Prior Parameters</b><br><br>
-             The prior estimate of the number socks is modeled with a
-             Negative Binomial distribtion. Prior mean and
-             Prior sample size control the shape of the negative binomial
-             distribution. Of course socks often go missing. The fraction of
-             socks that are paired is modeled with a Beta function. Fraction
-             of socks controls the shape of the Beta. Finally you can pick
-             how many unique socks you see from the dryer. <br><br>
+            <b>Prior Parameters</b><br>
+            The prior estimate of the number socks is modeled with a
+            Negative Binomial distribtion. Prior mean and
+            Prior sample size control the shape of the negative binomial
+            distribution. Of course socks often go missing. The fraction of
+            socks that are paired is modeled with a Beta function. Fraction
+            of socks controls the shape of the Beta. Finally you can pick
+            how many unique socks you see from the dryer. <br><br>
 
-             <b>Answer</b><br><br>
-             Once you are satisified click the button to show how many socks
-             are actually in Karl Broman\'s laundry <br><br>'
+            <b>Answer</b><br>
+            Once you are satisified, click the button to show how many socks
+            are actually in Karl Broman\'s laundry. <br><br>'
             )
     })
 
     output$answer = renderText(
-        '23 pairs and 3 upaired for a total of 45 socks.'
+        '23 pairs and 3 unpaired for a total of 45 socks.'
     )
 }
 
